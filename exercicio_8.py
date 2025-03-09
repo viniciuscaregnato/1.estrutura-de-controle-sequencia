0904698fc18c60672a7ad127f4f0bf577985f058
viniciuscaregnato <vinicius.cgarcia@gmail.com>
2025-03-09 14:45:40 -0300

    Add files via upload

---
+++ exercicio_8.py
@@ -0,0 +1,21 @@
+"""exercicio 8
+autor: vinicius
+instrução: resolva a equacao linear a.x = b
+versão: 0.0.1"""
+
+# Alocação de memoria
+
+x=""
+a=""
+b=""
+
+# Entrada de dados
+x = int(input("digite o valor de x: "))
+a = int(input("digite o valor de a: "))
+b = int(input("digite o valor de b: "))
+
+# Processamento de dados
+b = a*x
+
+#Saída de dados
+print("b = " + str(b))
