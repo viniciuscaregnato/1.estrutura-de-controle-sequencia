80e73e01834a15002028d22762fbef17a3b783e3
viniciuscaregnato <vinicius.cgarcia@gmail.com>
2025-03-09 14:33:27 -0300

    Add files via upload

---
+++ exercicio_3.py
@@ -0,0 +1,21 @@
+"""exercicio 3
+autor: vinicius
+instrução:  leia dois numeros do teclado, calcule a sua soma e imprima na tela seu resultado.
+versão: 0.0.1"""
+# Alocação de memoria
+
+numero_1 = None
+numero_2 = None
+
+# Entrada de dados
+
+numero_1 = int(input("digite um numero: "))
+numero_2 = int(input("digite outro numero: "))
+
+# Processamento de dados
+
+resultado = numero_1 + numero_2
+
+
+#Saída de dados
+print(resultado)
