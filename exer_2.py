f4f8c4542277ef1cb159af817632845728eed3e0
viniciuscaregnato <vinicius.cgarcia@gmail.com>
2025-03-09 14:34:38 -0300

    Add files via upload

---
+++ exer_2.py
@@ -0,0 +1,19 @@
+"""exercicio 2
+instrução: armazene os numeros 2 e 3, calcule a sua soma e imprima na tela o resultado.
+autor: vinicius
+versao: 0.0.1"""
+
+# Alocação de memoria
+
+numero_2 = 2
+numero_3 = 3
+
+# Entrada de dados
+
+# Processamento de dados
+
+resultado = numero_2 + numero_3
+
+#Saída de dados
+
+print(resultado)
